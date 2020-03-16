686892c0bb9d2890fb06027f280b1c73bf47c3a5
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-16 22:09:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,11 +21,7 @@ class Scraper
   end
   
   def get_courses
-    doc = Nokogiri::HTML(open('http://learn-co-curriculum.github.io/site-for-scraping/courses'))
-    doc.css('.post') #collection of course offerings
-    doc.css('.post').first.css("h2").text #title of course
-    doc.css('.post').first.css(".date").text #schedule of course
-    doc.css('.post').first.css("p").text #description of course
+    self.get_page.css('.post')
     
   end
   
@@ -36,6 +32,12 @@ class Scraper
 
   def print_courses
   end
+  
+  doc = Nokogiri::HTML(open('http://learn-co-curriculum.github.io/site-for-scraping/courses'))
+    doc.css('.post') #collection of course offerings
+    doc.css('.post').first.css("h2").text #title of course
+    doc.css('.post').first.css(".date").text #schedule of course
+    doc.css('.post').first.css("p").text #description of course
   
   
   
